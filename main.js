b3738c9bb1414a1f54b9b0389c53525cd73dd6d4
jennydeg <jenny.degling@gmail.com>
2015-02-09 11:39:47 +0100

    hade handledning, gjorde om allt :)))

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
-function showHappy()
-{
-
+function showHappy(){
     document.getElementById("sc-sad").style.display="none";
     document.getElementById("sc-love").style.display="none";
     document.getElementById("sc-happy").style.display="block";  
-     document.getElementById("container1").style.background="green";    
-
+	document.getElementById("container1").style.background="green";    
 }
-function showSad()
-{
 
+function showSad(){
     document.getElementById("sc-sad").style.display="block";
     document.getElementById("sc-love").style.display="none";
     document.getElementById("sc-happy").style.display="none";    
-
 }
-function showLove()
-{
 
+function showLove(){
     document.getElementById("sc-sad").style.display="none";
     document.getElementById("sc-love").style.display="block";
     document.getElementById("sc-happy").style.display="none";    
-
 }
+
 $( document ).ready(function() {
 	$("#clickinfo").click(function(){
 		$( "#info" ).fadeToggle();
-		var isVisible = $( "#info" ).is( ":visible" );
-		var isHidden = $( "#info" ).is( ":hidden" );
-		if(isVisible === true)
-			$("#infoimg").css("background", "#FF7F00");
-		if(isHidden == true)
-			$("#infoimg").css("background", "#bfbebc");
 	});
 	
 });
